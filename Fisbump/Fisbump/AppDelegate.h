21fa02c83c49d50fe53026becf7aba6ea1c1a6fe
谢远辉 <1152909011@qq.com>
2017-01-07 23:35:22 +0800

    第一次提交

---
+++ Fisbump/Fisbump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fisbump
+//
+//  Created by 米兰铁匠 on 17/1/7.
+//  Copyright © 2017年 米兰铁匠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fisbump/Fisbump/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fisbump
+//
+//  Created by 米兰铁匠 on 17/1/7.
+//  Copyright © 2017年 米兰铁匠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
